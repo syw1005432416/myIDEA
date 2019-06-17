bab49d99881d74299e3a2a98a6faf0cf7b7107ac
YEZIALi <1005432416@qq.com>
2019-06-17 20:05:10 +0800

    第二次添加到本地仓库

--- git_test01/src/com/yeziali/test/SayHello.java
+++ git_test01/src/com/yeziali/test/SayHello.java
@@ -3,5 +3,6 @@ package com.yeziali.test;
 public class SayHello {
     public static void main(String[] args) {
         System.out.println("hello");
+        System.out.println("hahahahahaha");
     }
 }
