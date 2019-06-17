e7c57cf1c29b3965dd8d19ae875945d065110a6e
YEZIALi <1005432416@qq.com>
2019-06-17 20:18:29 +0800

    第四次添加到本地仓库

--- git_test01/src/com/yeziali/test/SayHello.java
+++ git_test01/src/com/yeziali/test/SayHello.java
@@ -3,10 +3,5 @@ package com.yeziali.test;
 public class SayHello {
     public static void main(String[] args) {
         System.out.println("helloGit");
-        System.out.println("111111111111111");
-        System.out.println("123456");
-        System.out.println("hahahahahaha");
-        System.out.println("11111111111111");
-        System.out.println("2342423423524");
     }
 }
