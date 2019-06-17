8b75e7ddf592a5ce537172d7bf0431f438564a86
YEZIALi <1005432416@qq.com>
2019-06-17 20:15:34 +0800

    修改克隆文件

--- git_test01/src/com/yeziali/test/SayHello.java
+++ git_test01/src/com/yeziali/test/SayHello.java
@@ -4,6 +4,7 @@ public class SayHello {
     public static void main(String[] args) {
         System.out.println("helloGit");
         System.out.println("111111111111111");
+        System.out.println("123456");
         System.out.println("hahahahahaha");
     }
 }
